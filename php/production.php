bdd970312d5e416b2ac3a75b82fa3639390f797a
ArsVVS <arslan.tukhvatullin.834@gmail.com>
2022-12-02 19:10:21 +0300

    first commit

---
+++ php/production.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="../styles.css">
+
+  <title>Spin City Records</title>
+</head>
+
+<body>
+  <header class="header">
+    <div class="header__content container">
+      <nav class="header__navigation">
+        <img class="header__logo" src="/assets/logo.png" alt="logo">
+        <ul class="header__anchors">
+          <li class="header__anchor"><a href="index.php">Главная</a></li>
+          <li class="header__anchor"><a href="production.php">Магазин</a></li>
+          <li class="header__anchor"><a href="about.php">О нас</a></li>
+        </ul>
+      </nav>
+    </div>
+  </header>
+  <main class="page">
+    
+      
+  </main>
+  <footer class="footer">
+    <div class="footer__content container">
+      <div class="footer__contacts">
+        <div class="contacts__item">8 (495) 962-73-72</div>
+        <div class="contacts__item">spincity@records.ru</div>
+        <div class="contacts__item">м. Арбатская (Филевская), Большой Афанасьевский переулок д. 35-37, строение 4</div>
+      </div>
+      <div class="footer__copy">© 2022 <span class="colored">records.ru</span> — магазин виниловых пластинок.</div>
+    </div>
+  </footer>
+</body>
+
+</html>
